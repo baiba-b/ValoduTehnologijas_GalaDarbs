7e6a59fa01d9d31a080b1ad96a6f7d6f5b57c471
Baiba Bērziņa <77504706+baiba-b@users.noreply.github.com>
2026-06-18 02:27:02 +0300

    testa dati

---
+++ auto_import.py
@@ -0,0 +1,41 @@
+import HTML_ieguve_no_url
+import HTMLapstrade
+import EmocijuAnalize
+from database import init_db, save_article
+
+URLS = [
+    "https://www.lsm.lv/raksts/kas-notiek-latvija/raksti/17.06.2026-kas-notiek-latvija-aptauja-valsts-prezidenta-edgara-rinkevica-darbu-atzinigak-verte-jauniesi-un-augstu-ienakumu-sanemeji.a651894/",
+    "https://www.lsm.lv/raksts/zinas/latvija/17.06.2026-veselibas-ministrija-uzdod-stradina-slimnicai-risinat-sastregumus-uznemsana.a651929/",
+    "https://www.lsm.lv/raksts/sports/futbols/17.06.2026-pirmie-varti-un-punkts-pasaules-kausa-kongo-dr-futbolisti-parsteidz-portugali-un-spele-neizskirti.a651959/",
+    "https://www.delfi.lv/sports/44523532/pasaules-kauss-futbola/120123173/anglijas-futbolisti-nospiez-horvatiju-un-uzvar-rezultativa-pasaules-kausa-cina",
+    "https://www.delfi.lv/46713439/arzemes/120123152/dzivibas-atnemsana-vinam-ir-sports-longailendas-serijveida-slepkavam-piespriez-muza-ieslodzijumu",
+    "https://www.delfi.lv/46713439/arzemes/120123161/pusaudzi-eiropas-savieniba-socialos-tiklus-verte-pozitivak-neka-vecaki-liecina-aptauja",
+    "https://www.delfi.lv/46713439/arzemes/120123160/kostas-birojam-bijusi-diplomatiska-sazina-ar-kremli-pazino-es-amatpersona"
+]
+
+
+init_db()
+
+for url in URLS:
+    print("\nApstrādā:", url)
+
+    raksts = HTML_ieguve_no_url.get_body_html(url)
+
+    site = raksts["site"]
+    category = raksts["category"]
+    html = raksts["html"]
+
+    teksts = HTMLapstrade.html_to_txt(html)
+
+    emocijas = EmocijuAnalize.analizet_emocijas(teksts)
+
+    save_article(
+        url,
+        site,
+        category,
+        str(emocijas)
+    )
+
+    print("Saglabāts:", site, category)
+
+print("\nVisi URL apstrādāti.")
