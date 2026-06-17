d50e67d22fc607ce2b9b762a6e6877f3e7b7ce51
Baiba Bērziņa <77504706+baiba-b@users.noreply.github.com>
2026-06-17 22:35:09 +0300

    prieksapstrade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import HTML_ieguve_no_url
 import HTMLapstrade
+import prieksapstrade
+
+prieksapstrade.load_stoplist("data/stoplist.txt")
 
 url1 = input("Pirmais URL: ")
 url2 = input("Otrais URL: ")
@@ -10,10 +13,38 @@ raksts2 = HTML_ieguve_no_url.get_body_html(url2)
 site1 = raksts1["site"]
 site2 = raksts2["site"]
 
-teksts1 = HTMLapstrade.html_to_txt(raksts1["html"])
-teksts2 = HTMLapstrade.html_to_txt(raksts2["html"])
+html1 = raksts1["html"]
+html2 = raksts2["html"]
+
+# HTML -> tīrs teksts
+teksts1 = HTMLapstrade.html_to_txt(html1)
+teksts2 = HTMLapstrade.html_to_txt(html2)
+
+# Teksts -> tekstvienības (tokeni)
+tekstvienibas1 = prieksapstrade.tokenize(teksts1)
+tekstvienibas2 = prieksapstrade.tokenize(teksts2)
 
 print("\n=== Pirmais raksts ===")
 print("Vietne:", site1)
-# print(raksts1)
-# print(teksts1)
+print("Tekstvienības:")
+print(tekstvienibas1)
+
+print("\n=== Otrais raksts ===")
+print("Vietne:", site2)
+print("Tekstvienības:")
+print(tekstvienibas2)
+
+#Šo var neņemt - izveido biežumsarakstu
+
+from collections import Counter
+
+freq1 = Counter(tekstvienibas1)
+freq2 = Counter(tekstvienibas2)
+
+print("\nTop 20 tekstvienības 1. rakstā:")
+for token, count in freq1.most_common(20):
+    print(f"{token}: {count}")
+
+print("\nTop 20 tekstvienības 2. rakstā:")
+for token, count in freq2.most_common(20):
+    print(f"{token}: {count}")
